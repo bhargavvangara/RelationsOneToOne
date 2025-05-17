5c8754cbc2a61108b154c4ea21c729a1a7570f73
bhargavvangara <bhargavvangara5@gmail.com>
2025-05-17 16:37:58 +0530

    initial commit

---
+++ src/main/java/com/jpademo/relations/onetoone/demo/RelationsOneToOneApplication.java
@@ -0,0 +1,13 @@
+package com.jpademo.relations.onetoone.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelationsOneToOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelationsOneToOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpademo/relations/onetoone/demo/RelationsOneToOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpademo.relations.onetoone.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelationsOneToOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
